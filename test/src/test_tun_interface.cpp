8f14d8c37cf00dea9bd3e01a9d6c4c38e9896257
Jeppe Krigslund <krigslund@steinwurf.com>
2017-08-22 10:05:29 +0200

    modifying test to expect a failure

--- test/src/test_tun_interface.cpp
+++ test/src/test_tun_interface.cpp
@@ -10,11 +10,13 @@
 
 #include <boost/asio.hpp>
 
-TEST(test_tun_interface, construct)
+TEST(test_tun_interface, construct_no_su_fail)
 {
     boost::asio::io_service io;
     std::error_code error;
     std::string name("testtun");
+
+    // This should fail as super user permissions are needed by defaul
     auto t = tunnel::tun_interface::make_tun_interface(io, name, error);
-    ASSERT_FALSE(error);
+    ASSERT_TRUE(bool(error));
 }
