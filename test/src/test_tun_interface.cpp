9a471f24b1240f50ab0212fd54bf2a0cd4a73f0c
Jeppe Pihl <jpihl08@gmail.com>
2021-05-18 10:09:53 +0200

    remove super user assumption

--- test/src/test_tun_interface.cpp
+++ test/src/test_tun_interface.cpp
@@ -6,11 +6,9 @@
 #include <gtest/gtest.h>
 #include <tunnel/tun_interface.hpp>
 
-TEST(test_tun_interface, construct_no_su_fail)
+TEST(test_tun_interface, construct)
 {
     std::error_code error;
     tunnel::tun_interface t;
-    // This should fail as super user permissions are needed by default
-    t.create("", error);
-    ASSERT_TRUE(bool(error));
+    (void)t;
 }
