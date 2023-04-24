5c488a14b3eac896bc79a1b7858742da8f064d11
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2023-04-24 10:27:23 +0200

    working on it

--- src/tunnel/platform_unsupported/tun_interface.hpp
+++ src/tunnel/platform_unsupported/tun_interface.hpp
@@ -5,6 +5,81 @@ namespace platform_unsupported
 {
 struct tun_interface
 {
+    void create(const std::string& interface_name, std::error_code& error)
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
+    void rename(const std::string& interface_name, std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
+
+    void set_owner(const std::string& owner, std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
+
+    void set_group(const std::string& group, std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
+
+    std::string owner(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+        return "";
+    }
+
+    std::string interface_name(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+        return "";
+    }
+
+    bool is_up(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+        return false;
+    }
+
+    bool is_down(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+        return false;
+    }
+
+    void up(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
+
+    void down(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
+
+    bool is_persistent(std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+        return false;
+    }
+
+    void set_persistent(bool persistent, std::error_code& error) const
+    {
+        error = std::make_error_code(std::errc::not_supported);
+        throw error;
+    }
 }
 }
 }
