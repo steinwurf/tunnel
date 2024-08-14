116daa16106cc913e2c308b7ecaa01720080129f
suhrm <suhrminator@gmail.com>
2024-08-14 14:26:14 +0200

    use a config struct instead

---
+++ src/tunnel/interface_config.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+struct config
+
+{
+    /// The name of the interface
+    std::string interface_name;
+
+    /// If true the interface will be created with Virtual Network
+    /// Device Header enabled.
+    bool vnet_hdr = false;
+
+    /// If true the interface will be created with IFF_NO_PI enabled.
+    bool iff_no_pi = false;
+};
